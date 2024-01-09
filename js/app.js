6cd5a286a2a2e64a2dabab9454aae43bfc7184df
zalish-shawon <zalish.shawon@gmail.com>
2024-01-09 17:02:58 +0600

    added all

---
+++ js/app.js
@@ -0,0 +1,131 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.getElementById('list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'BURGER',
+        image: 'https://media.istockphoto.com/id/1154731746/photo/cheeseburger-with-cola-and-french-fries.jpg?s=612x612&w=0&k=20&c=DfuI7KyMDIF8_JH66oAhIZLOgL4RRXulfv24PQ5vTEo=',
+        price: 120
+    },
+    {
+        id: 2,
+        name: 'PiZZA',
+        image: 'https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D',
+        price: 180
+    },
+    {
+        id: 3,
+        name: 'DESSERT',
+        image: 'https://images.ctfassets.net/iyiurthvosft/featured-img-of-post-219893/4313f0e89c58718d5bd81d3aad7697bc/featured-img-of-post-219893.jpg?fm=jpg&fl=progressive&q=50&w=1200',
+        price: 220
+    },
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <div class="relative my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+                <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
+                  <img class="object-cover" src=${value.image} alt="product image" />
+                  <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
+                </a>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="title text-xl tracking-tight text-slate-900">${value.name}</h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span class="price text-3xl font-bold text-slate-900">${value.price.toLocaleString()}$</span>
+                      <span class="text-sm text-slate-900">/each</span>
+                    </p>
+                    
+                  </div>
+                  <button onclick="addToCard(${key})"  class="cartButton flex w-full items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300" onclick="addToCart()">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</button
+                  >
+                </div>
+              </div>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+
+
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    body.classList.add('active');
+    
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                
+                <div><img src="${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                    <button onclick="removeItem(${key})">Remove</button>
+                </div>
+                `;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
+function removeItem(key) {
+  // Remove the item from listCards
+  delete listCards[key];
+  reloadCard();
+}
+
+
+
+
+
+
+/* 
+
+
+*/
